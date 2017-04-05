45c50ed2f95886fcc2312cd9f96f93ec8bb92c51
bvellek <bvellek@uw.edu>
2017-04-05 13:00:23 -0700

    add selector

--- public/js/polyfillDetails.js
+++ public/js/polyfillDetails.js
@@ -18,7 +18,7 @@ void (function (root, factory) {
     'html.no-details ' + DETAILS + ':not([open]) > :not(' + SUMMARY + ') { display: none; }\n' +
     'html.no-details ' + DETAILS + ' > ' + SUMMARY + ':before { content: "▶"; display: inline-block; font-size: .8em; width: 1.5em; }\n' +
     'html.no-details ' + DETAILS + '[open] > ' + SUMMARY + ':before { content: "▼"; }' +
-    DETAILS + SUMMARY + '::-webkit-details-marker { display:none; }')
+    'html.no-details ' + DETAILS + SUMMARY + '::-webkit-details-marker { display:none; }')
 
   /*
    * Click handler for `<summary>` tags
